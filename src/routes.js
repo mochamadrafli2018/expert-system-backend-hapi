0e27c4f6709b116c38f49e0b738a444fc608767e
mochamadrafli2018 <mochamadrafli2018@mail.ugm.ac.id>
2021-09-14 21:35:30 +0700

    update

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,9 @@
-const {getNotes, getNoteById} = require('./handlerGet')
-const {addNote} = require('./handlerPost')
-
+const {DataProcessing} = require('./handlerPost');
 const routes = [
   {
-    method: 'GET',
-    path: '/notes',
-    handler: getNotes,
-  },
-  {
-    method: 'GET',
-    path: '/notes/{id}',
-    handler: getNoteById,
-  },
-  {
     method: 'POST',
-    path: '/notes',
-    handler: addNote,
+    path: '/diagnose',
+    handler: DataProcessing,
   },
-]
-
+];
 module.exports = routes;
