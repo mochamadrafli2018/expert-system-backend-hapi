8407ceae2de233567e30509426c85cee268385ea
mochamadrafli2018 <mochamadrafli2018@mail.ugm.ac.id>
2021-09-17 22:55:17 +0700

    update routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,21 @@
 const {DataProcessing} = require('./handlerPost');
+
 const routes = [
   {
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      const response = h.response({
+        status: 'success',
+        message: 'Welcome to web app service to diagnostic eye disease',
+      });
+      response.code(200);
+      return response;
+    },
+  },
+  {
     method: 'POST',
-    path: '/diagnose',
+    path: '/',
     handler: DataProcessing,
   },
 ];
